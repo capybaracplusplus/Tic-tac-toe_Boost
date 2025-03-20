2674a9fd052bb2c37fa1ba083cda5f82429e4a9a
Capy <dimak12k@yandex.ru>
2025-03-20 06:03:00 +0300

    add HTTP response class

---
+++ src/utils/http_response.cpp
@@ -0,0 +1,40 @@
+#include <http_response.h>
+
+HttpResponse::HttpResponse(int status) : status_code(status) {
+    headers["Content-Type"] = "text/plain";
+}
+
+void HttpResponse::set_status(int code) {
+    status_code = code;
+}
+
+void HttpResponse::set_header(const std::string& key, const std::string& value) {
+    headers[key] = value;
+}
+
+void HttpResponse::set_body(const std::string& body) {
+    response_body = body;
+    headers["Content-Length"] = std::to_string(body.size());
+}
+
+std::string HttpResponse::to_string() const {
+    std::ostringstream response;
+    response << "HTTP/1.1 " << status_code << " " << get_status_message() << "\r\n";
+    for (const auto& [key, value] : headers) {
+        response << key << ": " << value << "\r\n";
+    }
+    response << "\r\n" << response_body;
+    return response.str();
+}
+
+std::string HttpResponse::get_status_message() const {
+    switch (status_code) {
+        case 200: return "OK";
+        case 400: return "Bad Request";
+        case 401: return "Unauthorized";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 500: return "Internal Server Error";
+        default: return "Unknown";
+    }
+}
---
+++ src/utils/http_response.h
@@ -0,0 +1,26 @@
+#ifndef HTTP_RESPONSE_H
+#define HTTP_RESPONSE_H
+
+#include <string>
+#include <unordered_map>
+#include <sstream>
+
+class HttpResponse {
+public:
+    explicit HttpResponse(int status = 200);
+
+    void set_status(int code);
+    void set_header(const std::string& key, const std::string& value);
+    void set_body(const std::string& body);
+
+    std::string to_string() const;
+
+private:
+    int status_code;
+    std::unordered_map<std::string, std::string> headers;
+    std::string response_body;
+
+    std::string get_status_message() const;
+};
+
+#endif // HTTP_RESPONSE_H
