5eaec95ce2632ce1e2866543282c72bef6dbd270
Capy <dimak12k@yandex.ru>
2025-03-21 04:28:30 +0300

    update main func

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include <tcp_server.h>
 
-int main() {
-    try {
-        boost::asio::io_context io_context;
-        Server server(io_context, 8080);
-
-        io_context.run();
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    return 0;
+int main() try {
+    boost::asio::io_context io_context;
+    Server server(io_context, 8080);
+    io_context.run();
+} catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
 }
